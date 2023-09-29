bb0cdd23da67b3e1c88dc55c04ad6c1855682622
ArcyCola <arcycruz14@gmail.com>
2023-09-28 23:58:51 -0500

    completed mp4

--- MP4/template.cu
+++ MP4/template.cu
@@ -11,12 +11,54 @@
   } while (0)
 
 //@@ Define any useful program-wide constants here
-
+#define MASK_WIDTH 3
+#define MASK_RADIUS 1
+#define TILE_WIDTH 8
+#define BLOCK_WIDTH (TILE_WIDTH + MASK_WIDTH - 1)
 //@@ Define constant memory for device kernel here
+__constant__ float M[MASK_WIDTH][MASK_WIDTH][MASK_WIDTH];
+
 
 __global__ void conv3d(float *input, float *output, const int z_size,
                        const int y_size, const int x_size) {
   //@@ Insert kernel code here
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int tz = threadIdx.z;
+
+  int xo = blockIdx.x * TILE_WIDTH + tx;
+  int yo = blockIdx.y * TILE_WIDTH + ty;
+  int zo = blockIdx.z * TILE_WIDTH + tz;
+
+  int xi = xo - MASK_RADIUS;
+  int yi = yo - MASK_RADIUS;
+  int zi = zo - MASK_RADIUS;
+
+  __shared__ float input_ds[BLOCK_WIDTH][BLOCK_WIDTH][BLOCK_WIDTH];
+
+  // copy data from global memory to shared memory
+  if ((xi >= 0) && (xi < x_size) && (yi >= 0) && (yi < y_size) && (zi >= 0) && (zi < z_size)) {
+      input_ds[tz][ty][tx] = input[zi * (y_size * x_size) + yi * (x_size) + xi];
+  }
+  else {
+      input_ds[tz][ty][tx] = 0.0;
+  }
+
+  __syncthreads();
+
+  if (tx < TILE_WIDTH && ty < TILE_WIDTH && tz < TILE_WIDTH && xo < x_size && yo < y_size && zo < z_size) {
+
+    float Pvalue = 0.0;
+
+    for (int i = 0; i < MASK_WIDTH; i++) {
+      for (int j = 0; j < MASK_WIDTH; j++) { 
+        for (int k = 0; k < MASK_WIDTH; k++) {
+          Pvalue += M[i][j][k] * input_ds[tz + i][ty + j][tx + k];
+        }
+      }
+    }
+    output[zo * (y_size * x_size) + yo * (x_size) + xo] = Pvalue;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -53,6 +95,10 @@ int main(int argc, char *argv[]) {
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
   // because the first  three elements were the dimensions
+  int tensorLength = inputLength - 3;
+  cudaMalloc((void **)&deviceInput, tensorLength * sizeof(float));
+  cudaMalloc((void **)&deviceOutput, tensorLength * sizeof(float));
+
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
@@ -60,12 +106,21 @@ int main(int argc, char *argv[]) {
   // Recall that the first three elements of hostInput are dimensions and
   // do
   // not need to be copied to the gpu
+  cudaMemcpy(deviceInput, &hostInput[3], tensorLength * sizeof(float), cudaMemcpyHostToDevice);
+
+  cudaMemcpyToSymbol(M, hostKernel, MASK_WIDTH * MASK_WIDTH * MASK_WIDTH * sizeof(float));
+
   wbTime_stop(Copy, "Copying data to the GPU");
 
   wbTime_start(Compute, "Doing the computation on the GPU");
   //@@ Initialize grid and block dimensions here
+  dim3 DimGrid(ceil(((float)x_size) / TILE_WIDTH), ceil(((float)y_size) / TILE_WIDTH), ceil(((float)z_size) / TILE_WIDTH));
+  dim3 DimBlock(BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH);
 
   //@@ Launch the GPU kernel here
+  conv3d<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput, z_size, y_size, x_size);
+
+
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
@@ -73,6 +128,8 @@ int main(int argc, char *argv[]) {
   //@@ Copy the device memory back to the host here
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
+  cudaMemcpy(&hostOutput[3], deviceOutput, tensorLength * sizeof(float), cudaMemcpyDeviceToHost);
+
   wbTime_stop(Copy, "Copying data from the GPU");
 
   wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
